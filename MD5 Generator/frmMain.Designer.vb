7eea4c76151e5ff1c375ab23d8da13da7b58c99f
mithwick93 <mithwick93@gmail.com>
2015-05-01 13:08:09 +0530

    First commit

---
+++ MD5 Generator/frmMain.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.btnGenerate = New System.Windows.Forms.Button()
+		Me.txtFile = New System.Windows.Forms.TextBox()
+		Me.lblMD5 = New System.Windows.Forms.Label()
+		Me.txtHashMD5 = New System.Windows.Forms.TextBox()
+		Me.openFileDialog = New System.Windows.Forms.OpenFileDialog()
+		Me.txtHashSHA1 = New System.Windows.Forms.TextBox()
+		Me.lblSHA1 = New System.Windows.Forms.Label()
+		Me.txtHashSHA256 = New System.Windows.Forms.TextBox()
+		Me.lblSHA256 = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'btnGenerate
+		'
+		Me.btnGenerate.Location = New System.Drawing.Point(407, 16)
+		Me.btnGenerate.Name = "btnGenerate"
+		Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+		Me.btnGenerate.TabIndex = 0
+		Me.btnGenerate.Text = "Generate"
+		Me.btnGenerate.UseVisualStyleBackColor = True
+		'
+		'txtFile
+		'
+		Me.txtFile.Location = New System.Drawing.Point(12, 18)
+		Me.txtFile.Name = "txtFile"
+		Me.txtFile.Size = New System.Drawing.Size(378, 20)
+		Me.txtFile.TabIndex = 1
+		'
+		'lblMD5
+		'
+		Me.lblMD5.AutoSize = True
+		Me.lblMD5.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblMD5.Location = New System.Drawing.Point(12, 66)
+		Me.lblMD5.Name = "lblMD5"
+		Me.lblMD5.Size = New System.Drawing.Size(54, 18)
+		Me.lblMD5.TabIndex = 2
+		Me.lblMD5.Text = "MD5 : "
+		'
+		'txtHashMD5
+		'
+		Me.txtHashMD5.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtHashMD5.Location = New System.Drawing.Point(86, 63)
+		Me.txtHashMD5.Name = "txtHashMD5"
+		Me.txtHashMD5.ReadOnly = True
+		Me.txtHashMD5.Size = New System.Drawing.Size(393, 26)
+		Me.txtHashMD5.TabIndex = 3
+		'
+		'openFileDialog
+		'
+		Me.openFileDialog.Title = "Open File"
+		'
+		'txtHashSHA1
+		'
+		Me.txtHashSHA1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtHashSHA1.Location = New System.Drawing.Point(86, 95)
+		Me.txtHashSHA1.Name = "txtHashSHA1"
+		Me.txtHashSHA1.ReadOnly = True
+		Me.txtHashSHA1.Size = New System.Drawing.Size(393, 26)
+		Me.txtHashSHA1.TabIndex = 5
+		'
+		'lblSHA1
+		'
+		Me.lblSHA1.AutoSize = True
+		Me.lblSHA1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblSHA1.Location = New System.Drawing.Point(12, 98)
+		Me.lblSHA1.Name = "lblSHA1"
+		Me.lblSHA1.Size = New System.Drawing.Size(58, 18)
+		Me.lblSHA1.TabIndex = 4
+		Me.lblSHA1.Text = "SHA1 :"
+		'
+		'txtHashSHA256
+		'
+		Me.txtHashSHA256.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtHashSHA256.Location = New System.Drawing.Point(86, 127)
+		Me.txtHashSHA256.Name = "txtHashSHA256"
+		Me.txtHashSHA256.ReadOnly = True
+		Me.txtHashSHA256.Size = New System.Drawing.Size(393, 26)
+		Me.txtHashSHA256.TabIndex = 7
+		'
+		'lblSHA256
+		'
+		Me.lblSHA256.AutoSize = True
+		Me.lblSHA256.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblSHA256.Location = New System.Drawing.Point(12, 130)
+		Me.lblSHA256.Name = "lblSHA256"
+		Me.lblSHA256.Size = New System.Drawing.Size(76, 18)
+		Me.lblSHA256.TabIndex = 6
+		Me.lblSHA256.Text = "SHA256 :"
+		'
+		'frmMain
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(484, 197)
+		Me.Controls.Add(Me.txtHashSHA256)
+		Me.Controls.Add(Me.lblSHA256)
+		Me.Controls.Add(Me.txtHashSHA1)
+		Me.Controls.Add(Me.lblSHA1)
+		Me.Controls.Add(Me.txtHashMD5)
+		Me.Controls.Add(Me.lblMD5)
+		Me.Controls.Add(Me.txtFile)
+		Me.Controls.Add(Me.btnGenerate)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+		Me.MaximizeBox = False
+		Me.Name = "frmMain"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Hash Generator"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+	Friend WithEvents btnGenerate As System.Windows.Forms.Button
+	Friend WithEvents txtFile As System.Windows.Forms.TextBox
+	Friend WithEvents lblMD5 As System.Windows.Forms.Label
+	Friend WithEvents txtHashMD5 As System.Windows.Forms.TextBox
+	Friend WithEvents openFileDialog As System.Windows.Forms.OpenFileDialog
+	Friend WithEvents txtHashSHA1 As System.Windows.Forms.TextBox
+	Friend WithEvents lblSHA1 As System.Windows.Forms.Label
+	Friend WithEvents txtHashSHA256 As System.Windows.Forms.TextBox
+	Friend WithEvents lblSHA256 As System.Windows.Forms.Label
+
+End Class
---
+++ MD5 Generator/frmMain.vb
@@ -0,0 +1,139 @@
+﻿Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Public Class frmMain
+
+#Region " Functions "
+	Delegate Sub get_md5_Delegate(ByVal fileName As String)
+	Delegate Sub get_sha1_Delegate(ByVal fileName As String)
+	Delegate Sub get_sha256_Delegate(ByVal fileName As String)
+
+
+	Private Sub disableUI()
+		Me.Cursor = Cursors.WaitCursor
+		lblMD5.Enabled = False
+		lblSHA1.Enabled = False
+		lblSHA256.Enabled = False
+		txtHashMD5.Text = "Calculating ,Please wait ...."
+		txtHashSHA1.Text = ""
+		txtHashSHA256.Text = ""
+		btnGenerate.Enabled = False
+		txtFile.Enabled = False
+		txtHashMD5.Enabled = False
+		txtHashSHA1.Enabled = False
+		txtHashSHA256.Enabled = False
+	End Sub
+
+	Private Sub enableUI()
+		Me.Cursor = Cursors.Default
+		lblMD5.Enabled = True
+		lblSHA1.Enabled = True
+		lblSHA256.Enabled = True
+		btnGenerate.Enabled = True
+		txtFile.Enabled = True
+		txtHashMD5.Enabled = True
+		txtHashSHA1.Enabled = True
+		txtHashSHA256.Enabled = True
+	End Sub
+
+	Private Sub openFile()
+		openFileDialog.Filter = "All Files (*.*)|*.*"
+		If (openFileDialog.ShowDialog() = DialogResult.OK) Then
+			txtFile.Text = openFileDialog.FileName
+		Else
+			Exit Sub
+		End If
+		If openFileDialog.FileName = "" Then Exit Sub
+		disableUI()
+
+		Dim threadGetHash_md5 As get_md5_Delegate = AddressOf get_md5
+		threadGetHash_md5.BeginInvoke(openFileDialog.FileName.ToString, Nothing, Nothing)
+
+		Dim threadGetHash_sha1 As get_sha1_Delegate = AddressOf get_sha1
+		threadGetHash_sha1.BeginInvoke(openFileDialog.FileName.ToString, Nothing, Nothing)
+
+		Dim threadGetHash_sha256 As get_sha256_Delegate = AddressOf get_sha256
+		threadGetHash_sha256.BeginInvoke(openFileDialog.FileName.ToString, Nothing, Nothing)
+	End Sub
+
+	Private Sub get_md5(ByVal fileName As String)
+		Dim hashMD5 = MD5.Create	
+		Dim hashValueMD5() As Byte
+		Dim fileStream As FileStream = File.OpenRead(fileName)
+		fileStream.Position = 0
+		hashValueMD5 = hashMD5.ComputeHash(fileStream)
+		fileStream.Close()
+		If Me.InvokeRequired Then
+			Me.Invoke(New get_md5_Delegate(AddressOf get_md5), fileName)
+		Else
+			txtHashMD5.Text = byteArrayToString(hashValueMD5)
+			enableUI()
+		End If
+	End Sub
+
+	Private Sub get_sha1(ByVal fileName As String)
+		Dim hashSHA1 = SHA1.Create()
+		Dim hashValueSHA1() As Byte
+		Dim fileStream As FileStream = File.OpenRead(fileName)
+		fileStream.Position = 0
+		hashValueSHA1 = hashSHA1.ComputeHash(fileStream)
+		fileStream.Close()
+		If Me.InvokeRequired Then
+			Me.Invoke(New get_sha1_Delegate(AddressOf get_sha1), fileName)
+		Else
+			txtHashSHA1.Text = byteArrayToString(hashValueSHA1)
+			enableUI()
+		End If
+	End Sub
+
+	Private Sub get_sha256(ByVal fileName As String)
+		Dim hashSHA256 = SHA256.Create()
+		Dim hashValueSHA256() As Byte
+		Dim fileStream As FileStream = File.OpenRead(fileName)
+		fileStream.Position = 0
+		hashValueSHA256 = hashSHA256.ComputeHash(fileStream)
+		fileStream.Close()
+
+		If Me.InvokeRequired Then
+			Me.Invoke(New get_sha256_Delegate(AddressOf get_sha256), fileName)
+		Else
+			txtHashSHA256.Text = byteArrayToString(hashValueSHA256)
+			enableUI()
+		End If
+	End Sub
+
+	Public Function byteArrayToString(ByVal array() As Byte) As String
+		Dim hexValue As String = ""
+		Dim i As Integer
+		For i = 0 To array.Length - 1
+			hexValue += array(i).ToString("X2")
+		Next i
+		Return hexValue.ToLower
+	End Function
+
+	Private Function getPlatformBit() As String
+		If IntPtr.Size > 4 Then
+			Return "x64"
+		End If
+		Return "x86"
+	End Function
+#End Region
+
+#Region " Form "
+	Private Sub frmMain_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+		Me.Icon = My.Resources.FileHash
+		Me.Text = Me.Text + " " + getPlatformBit()
+	End Sub
+#End Region
+
+#Region " GUI "
+	Private Sub btnGenerate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGenerate.Click
+		openFile()
+	End Sub
+
+	Private Sub txtFile_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtFile.DoubleClick
+		openFile()
+	End Sub
+#End Region
+
+End Class
